d62cec5b2c34c8ff46fcacc713a90a8b48f6a6ec
MatteoPaz <>
2021-09-11 06:11:05 +0000

    Basics of postform

--- src/comp/Postform.svelte
+++ src/comp/Postform.svelte
@@ -1,13 +1,23 @@
 <script>
   export let formactive = false;
-  let show = [false, false];
+  let show = [false, false, false, false];//Total render, fade, input render, input fade
+  let name = "";
+  let title = "";
+  let author = "";
+  let body = "";
+  let tags = ["Foo", "Bar", "Baz"];
   $: if(formactive) {
     show[0] = true;
     setTimeout(() => {
       show[1] = true;
     }, 10);
+    setTimeout(() => {
+      show[2] = true;
+      setTimeout(() => show[3] = true, 1);
+    }, 450);
   } else {
     show[1] = false;
+    show[2] = false;
     setTimeout(() => {
       show[0] = false;
     }, 400);
@@ -16,7 +26,26 @@
 
 {#if show[0]}
 <form class:show={show[1]} >
-
+  {#if show[2]}
+  <div class="inputs" class:show={show[3]} >
+  <label class="name">
+    Post as:
+    <input type="text" bind:value={name} />
+  </label>
+  <details>
+    <summary>Tags</summary>
+    <ul class="chips">
+     {#each tags as tag}
+      <li class="chip">{tag}</li>
+     {/each}
+    </ul>
+  </details>
+  <label class="header">
+    Post Title:
+    <input type="text" bind:value={title}>
+  </label>
+  </div>
+  {/if}
 </form>
 {/if}
 
@@ -39,6 +68,10 @@
   .show {
     width: 100%;
     opacity: 1;
+  }
+
+  .inputs {
+    transition: opacity 0.5s ease;
   }
 </style>
 
