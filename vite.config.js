dbb040ccb8c8850f9cc62c43d41c7ebe5d9de580
Matteo Paz <86904497+matteopaz@users.noreply.github.com>
2021-09-13 23:18:25 -0700

    Fixed vite-svelte import breaking change

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
-import svelte from '@sveltejs/vite-plugin-svelte';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 import viteCompression from 'vite-plugin-compression';
 
